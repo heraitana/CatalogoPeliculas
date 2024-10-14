89c9bd7cd7146de4162c26e4cb3e2c8cf08c5359
heraitana <108695552+heraitana@users.noreply.github.com>
2024-10-14 08:59:36 -0600

    Add files via upload

---
+++ CatalogoPeliculas/src/main/java/catalogo/Ficcion/Thriller.java
@@ -0,0 +1,18 @@
+package catalogo.Ficcion;
+
+public class Thriller extends Ficcion{
+    public Thriller(String titulo, String director, double calificacionIMDb, int year) {
+        super(titulo, director, calificacionIMDb, year,"", "Thriller");
+    }
+
+    @Override
+    public String toString() {
+        return "Thriller: " + super.toString();
+    }
+    @Override
+    public String getSubGenero() {
+        return "Thriller";
+    }
+
+
+}
